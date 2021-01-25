1c4a29b4f60e61a968127cbbc980820e0ccb5727
Vision.Mac <chao2.ma@changhong.com>
2021-01-26 00:20:54 +0800

    优化版本

---
+++ sei-datachange-api/src/main/java/com/changhong/sei/datachange/AppVersion.java
@@ -0,0 +1,16 @@
+package com.changhong.sei.datachange;
+
+import com.changhong.sei.core.BaseVersion;
+
+/**
+ * 实现功能：应用版本
+ *
+ * @author 马超(Vision.Mac)
+ * @version 1.0.00  2021-01-12 08:35
+ */
+public class AppVersion extends BaseVersion {
+
+    public AppVersion() {
+        super(AppVersion.class);
+    }
+}
